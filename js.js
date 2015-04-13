f2596c93b68c4c36a5ed87591074854d845b4708
Anthony <anthony.guev@gmail.com>
2015-04-13 12:18:36 +1000

    Added shortcode plugin, tested and working.

--- js.js
+++ js.js
@@ -1,41 +1,68 @@
 var x = document.getElementById("selectEpisodeNumber");
 
+//Preload the episode number from 1 to 500
 for (epNum = 1; epNum <= 500; ++epNum) {
   var option = document.createElement("option");
+  option.value = epNum;
   option.text = epNum;
-  x.add(option);
+    x.add(option);
+}
+
+var episodeNumber = getCookie("episodeNumber");
+console.log(episodeNumber);
+if ( isNaN(episodeNumber) ) {
+   $("#selectEpisodeNumber").val("default");
+}
+else {
+   $("#selectEpisodeNumber").val(episodeNumber);
 }
 
 $(document).ready(function(){
-   var dialog = document.getElementById("myDialog")
-
-   $("#dialogClose").click(function() {
-   });
-
 
 });
 
-//DIALOG MESSAGE
-function foo() {
- $( "#dialog-message" ).dialog({
-   modal: false,
-   buttons: {
-     Ok: function() {
-       $( this ).dialog( "close" );
-     }
-   }
- });
-}
+$(".time").click(function(e) {
+    console.log($(this).width());
+    console.log($(this).height());
 
-$(".time").click(function() {
-   foo();
-   alert($(this).position());
-   //$(this).val("return value");
+    $( "#dialog-message" ).dialog({
+	modal: false,
+	buttons: {
+	    Ok: function() {
+		$( this ).dialog( "close" );
+	    }
+	},
+	position: $(this).position()
+    });
+
+    $("#dialog-message").dialog("open");
 });
+
 
 
 $("#idInputNewBookmark").click(function() {
-   alert("noob");
-   $('#tableBookmark tr:last').after('<tr>hitr><tr>bye</tr>');
+   //$(this).prop('disabled', true);
+   //$('#tableBookmark tr:last').after('<tr>hitr><tr>bye</tr>');
    //$(this).val("return value");
 });
+
+$('#selectEpisodeNumber').change(function(){
+    var episodeNumber = $(this).val();
+    document.cookie="episodeNumber=" + episodeNumber; 
+   $("#inputForm").submit();
+});
+
+$("#inputSaveTitle").click(function() {
+    console.log("yea");
+});
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
--- podcastpop.php
+++ podcastpop.php
@@ -1,195 +1,335 @@
 <?php
 /*
-Plugin Name: Podcast Pop Bookmarks
-Plugin URI:  http://wordpress.org/extend/plugins/health-check/
-Description: Checks the health of your WordPress install
-Version:     0.1-alpha
-Author:      The Health Check Team
-Author URI:  http://wordpress.org/extend/plugins/health-check/
-Text Domain: health-check
-Domain Path: /lang
- */
-   function pcpb_install () {
-      global $wpdb;
-      $pcpb_db_version = "1.0";
+  Plugin Name: Podcast Pop Bookmarks
+  Plugin URI:  http://wordpress.org/extend/plugins/health-check/
+  Description: Checks the health of your WordPress install
+  Version:     0.1-alpha
+  Author:      The Health Check Team
+  Author URI:  http://wordpress.org/extend/plugins/health-check/
+  Text Domain: health-check
+  Domain Path: /lang
+*/
+/*
+function podcastpop_shortcode( $atts, $content = null)	{
 
-      $table_name = $wpdb->prefix . "pcpbplugin";
+	extract( shortcode_atts( array('message' => ''),
+                                    $atts ));
 
-      $charset_collate = $wpdb->get_charset_collate();
 
-      $sql = "CREATE TABLE $table_name (
-        id mediumint(9) NOT NULL AUTO_INCREMENT,
-        episodeNumber smallint NOT NULL,
-        startTime time NOT NULL DEFAULT '00:00:00',
-        text varchar(60) DEFAULT '' NOT NULL,
-        updatedAt timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        UNIQUE KEY id (id)
-      ) $charset_collate;";
 
-      require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-      dbDelta( $sql );
+    
+	// this will display our message before the content of the shortcode
+	return 'boobs ' . $message . 'boogers' . $content;
+}
+*/
+function pippin_example_shortcode( $atts, $content = null)	{
+    global $wpdb;
+    $table_plugin = $wpdb->prefix . "pcpbplugin";
+    $table_title  = $wpdb->prefix . "pcpbptitle";
 
-      add_option( 'pcpb_db_version', $pcpb_db_version );
+    extract( shortcode_atts( array(
+        'message' => '' /*episode Number???*/
+			), $atts 
+		) 
+	);
+    
+    $bookmarks = $wpdb->get_results("SELECT * FROM $table_plugin WHERE episodeNumber = " . 
+                                    $message . " ORDER BY startTime DESC");
 
-      $wpdb->insert( 
-         $table_name, 
-         array( 
+    $title = $wpdb->get_row("SELECT * FROM $table_title WHERE episodeNumber = " .
+                            $message);
+    $concat = "<table>
+    <thead>
+    <tr>
+    <th>Time</th>
+    <th>Bookmark Text</th>
+    </tr>
+    </thead>";
+    
+    foreach ($bookmarks as $bookmark) {
+        $concat .= "<tr>";
+        $concat .= "<td>";
+        $concat .= $bookmark->startTime;
+        $concat .= "</td>";
+        $concat .= "<td>";
+        $concat .= $bookmark->text;
+        $concat .= "</td>";
+        $concat .= "</tr>";
+    }
+
+    $concat .= "</table>";
+
+
+	// this will display our message before the content of the shortcode
+	return $concat;
+ 
+}
+add_shortcode('podcastpop', 'pippin_example_shortcode');
+
+/*add_shortcode('podcastpop', 'podcastpop_shortcode');*/
+
+/*
+  Create new database tables for plugin and title
+*/
+function pcpb_install () {
+    global $wpdb;
+    $pcpb_db_version = "1.0";
+
+    $pluginTable = $wpdb->prefix . "pcpbplugin";
+    $pluginTitle = $wpdb->prefix . "pcpbptitle";
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $pluginTable = "CREATE TABLE $pluginTable (
+     id mediumint(9) NOT NULL AUTO_INCREMENT,
+     episodeNumber smallint NOT NULL,
+     startTime time NOT NULL DEFAULT '00:00:00',
+     text varchar(60) DEFAULT '' NOT NULL,
+     updatedAt timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     UNIQUE KEY id (id)
+   ) $charset_collate;";
+
+    $pluginTitle = "CREATE TABLE $pluginTitle (
+     id mediumint(9) NOT NULL AUTO_INCREMENT,
+     episodeNumber smallint NOT NULL,
+     episodeTitle varchar(60) DEFAULT '' NOT NULL,
+     updatedAt timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     UNIQUE KEY id (id)
+   ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $pluginTable );
+    dbDelta( $pluginTitle );
+
+    add_option( 'pcpb_db_version', $pcpb_db_version );
+
+    $wpdb->insert( 
+        $table_name, 
+        array( 
             'episodeNumber' => 1,
             'startTime'     => '252525',
             'text'          => 'yo',
-         ) 
-      );
-   }
-   // Hook for adding admin menus
-   add_action('admin_menu', 'mt_add_pages');
-   register_activation_hook( __FILE__, 'pcpb_install' );
+        ) 
+    );
+}
 
-   function mt_add_pages() {
-       add_options_page(__('Test Settings','menu-test'), __('Test Settings','menu-test'), 'manage_options', 'testsettings', 'mt_settings_page');
-       add_menu_page("Hello World", "Podcast Pop Bookmarks", 'manage_options', 'mt-top-level-handle', 'mt_settings_page' );
-   }
+// Hook for adding admin menus
+add_action('admin_menu', 'mt_add_pages');
+register_activation_hook( __FILE__, 'pcpb_install' );
 
-   // mt_toplevel_page() displays the page content for the custom Test Toplevel menu
-   function podcastpop_bookmarks_toplevel_page() {
-   	if (!current_user_can('manage_options'))  {
-         wp_die( __('You do not have sufficient permissions to access this page.') );
-      }
-      echo "<h2> DOOOOOOOOOOD </h2>";
-   }
+function mt_add_pages() {
+    add_options_page(__('Test Settings','menu-test'), __('Test Settings','menu-test'), 'manage_options', 'testsettings', 'mt_settings_page');
+    add_menu_page("Hello World", "Podcast Pop Bookmarks", 'manage_options', 'mt-top-level-handle', 'mt_settings_page' );
+}
 
-
-      // mt_settings_page() displays the page content for the Test settings submenu
-   function mt_settings_page() {
-
-       //must check that the user has the required capability 
-       if (!current_user_can('manage_options'))
-       {
-         wp_die( __('You do not have sufficient permissions to access this page.') );
-       }
-
-       // variables for the field and option names 
-       $opt_name = 'mt_favorite_color';
-       $hidden_field_name = 'mt_submit_hidden';
-       $data_field_name = 'mt_favorite_color';
-
-       // Read in existing option value from database
-       $opt_val = get_option( $opt_name );
-
-       // See if the user has posted us some information
-       // If they did, this hidden field will be set to 'Y'
-       if( isset($_POST[ $hidden_field_name ]) && $_POST[ $hidden_field_name ] == 'Y' ) {
-           // Read their posted value
-           $opt_val = $_POST[ $data_field_name ];
-
-           // Save the posted value in the database
-           update_option( $opt_name, $opt_val );
-
-           // Put an settings updated message on the screen
-
-?>
-         <div class="updated"><p><strong>Settings Saved EOR</strong></p></div>
-         <?php
-
-       }
-
-       // Now display the settings editing screen
-
-       echo '<div class="wrap">';
-
-       // header
-
-       echo "<h2>" . "Podcast Pop Bookmarks" . "</h2>";
-
-       // settings form
-          
-      ?>
-      Episode Number
-      <select required id="selectEpisodeNumber"></select>
-      Title for Display
-      <input type="text" name="titleForDisplay" size=88>
-      <input id="inputSaveTitle" type="submit" name="Submit" class="btn btn-primary" value="Save" />
-      <hr/>
-      <form action="" method="POST">
-         Time <input class="time" name="inputTime" type="text" readonly>
-         Bookmark Text <input id="idInputBookmarkText" name="inputBookmarkText" type="text" size=50>
-      
-         <input id="idInputNewBookmark" type="submit" name="inputNewBookmark" class="btn btn-primary" value="+ New Bookmark" />
-
-      <?php
-         if ( isset($_POST['inputBookmarkText']) ) {
-            $x = $_POST['inputBookmarkText'];
-            echo $x;
-         }
-
-         if ( isset($_POST['inputTime']) ) {
-            $x = $_POST['inputTime'];
-            echo $x;
-         }
-      ?>
-
-      </form>
-
-      
-      Search:
-      <input id="inputSearchBookmark" type="text" />
-      <hr />
-
-      <!-- bookmarks table -->
-      <table id="tableBookmark" class="table table-striped">
-         <thead>
-            <tr>
-               <th>Time</th>
-               <th>Bookmark Text</th>
-            </tr>
-         </thead>
-         <tr>
-            <td>hi</td>
-            <td>bye</td>
-         </tr>
-         <tr>
-            <td>hi</td>
-            <td>bye</td>
-         </tr>
-         <tr>
-            <td>hi</td>
-            <td>bye</td>
-         </tr>
-      </table>
-
-      <input id="spinner" name="value">
-      <input type="number" min="0" max="60" step="1" />
-
-      </div>
-
-      <script>
-      console.log("poop");
-      console.log("<?php global $wpdb; echo $wpdb->prefix ?>");
-      </script>
-
-//DIALOG MESSAGE
-<div id="dialog-message" title="Pick a Time">
-  <p>
-    <span class="ui-icon ui-icon-circle-check" style="float:left; margin:0 7px 50px 0;"></span>
-    Your files have downloaded successfully into the My Downloads folder.
-  </p>
-  <p>
-    Currently using <b>36% of your storage space</b>.
-  </p>
-</div>
+// mt_toplevel_page() displays the page content for the custom Test Toplevel menu
+function podcastpop_bookmarks_toplevel_page() {
+    if (!current_user_can('manage_options'))  {
+        wp_die( __('You do not have sufficient permissions to access this page.') );
+    }
+    echo "<h2> DOOOOOOOOOOD </h2>";
+}
 
 
-   </html>
-<head>
-   <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-   <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-</head>
-      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-      <script src="https://code.jquery.com/ui/1.11.3/jquery-ui.min.js"></script>
-      <?php $dir = plugins_url() . "/podcastpop" . "/js.js"; ?>
-      <script src="<?php echo $dir ?>"></script>
+// mt_settings_page() displays the page content for the Test settings submenu
+function mt_settings_page() {
+
+    //must check that the user has the required capability 
+    if (!current_user_can('manage_options'))
+    {
+        wp_die( __('You do not have sufficient permissions to access this page.') );
+    }
+
+    // variables for the field and option names 
+    $opt_name = 'mt_favorite_color';
+    $hidden_field_name = 'mt_submit_hidden';
+    $data_field_name = 'mt_favorite_color';
+
+    // Read in existing option value from database
+    $opt_val = get_option( $opt_name );
+
+    // See if the user has posted us some information
+    // If they did, this hidden field will be set to 'Y'
+    if( isset($_POST[ $hidden_field_name ]) && $_POST[ $hidden_field_name ] == 'Y' ) {
+        // Read their posted value
+        $opt_val = $_POST[ $data_field_name ];
+
+        // Save the posted value in the database
+        update_option( $opt_name, $opt_val );
+
+        // Put an settings updated message on the screen
+
+        ?>
+        <div class="updated"><p><strong>Settings Saved EOR</strong></p></div>
+<?php
+
+    }
+
+    // Now display the settings editing screen
+
+    echo '<div class="wrap">';
+
+    // header
+
+    echo "<h2>" . "Podcast Pop Bookmarks" . "</h2>";
+
+    // settings form
+       
+    ?>
+    <form id="inputForm" method="POST">
+    Episode Number
+        <select required name="episodeNumber" id="selectEpisodeNumber">
+    <option value="default">Select an episode...</option>
+    </select>
+
+    Title for Display 
+        <input type="text" name="titleForDisplay" value="
+<?php 
+global $wpdb;
+$episodeNumber = $_COOKIE['episodeNumber'];
+$table_title  = $wpdb->prefix . "pcpbptitle";
+
+$title = $wpdb->get_row("SELECT * FROM $table_title WHERE episodeNumber = " .
+   $episodeNumber);
+
+echo $title->episodeTitle;
+?>" size=88>
+                                     <input id="inputSaveTitle" type="submit" name="inputSaveTitle" class="btn btn-primary" value="Save" />
+    <hr/>
+    Time <input id="theTime" class="time" name="inputTime" type="text">
+
+                  Bookmark Text <input id="idInputBookmarkText" name="inputBookmarkText" type="text" size=50>
+   
+                  <input id="idInputNewBookmark" type="submit" name="inputNewBookmark" class="btn btn-primary" value="+ New Bookmark"/>
+    </form>
+
+<?php
+    $errorMessage = "";
+    $title = $_POST['titleForDisplay'];
+    $episodeNumber = $_POST['episodeNumber'];
+    $bmtext = "";
+    $inputTime = "";
+              
+    if (isset($_POST['inputNewBookmark'])) {
+        if ( !empty($_POST['inputBookmarkText']) &&
+             !empty($_POST['inputTime'])) {
+            $bmtext = $_POST['inputBookmarkText'];
+            $inputTime = $_POST['inputTime'];
+
+            global $wpdb;
+            $table_name = $wpdb->prefix . "pcpbplugin";
+
+            $wpdb->insert( 
+                $table_name, 
+                array( 
+                    'episodeNumber' => $episodeNumber,
+                    'startTime'     => $inputTime,
+                    'text'          => $bmtext,
+                ) 
+            );
+            echo "<script>location='admin.php?page=mt-top-level-handle'</script>";
+        }
+        else
+            echo "Both Time and Bookmark text need to be set.";
+    }
+    else if (isset($_POST['inputSaveTitle'])) {
+        if ( !empty($_POST['titleForDisplay']) ) {
+            global $wpdb;
+            $table_name = $wpdb->prefix . "pcpbptitle";
+
+            $title = $_POST['titleForDisplay'];
+            $wpdb->query("DELETE FROM $table_name WHERE `episodeNumber` = " . $episodeNumber);
+                           
+            $wpdb->insert(
+                $table_name, 
+                array(
+                    'episodeNumber' => $episodeNumber,
+                    'episodeTitle'  => $title,
+                )
+            );
+
+            echo "<script>location='admin.php?page=mt-top-level-handle'</script>";
+        }
+        else {
+            echo "Please enter a valid title!";
+        }
+    }
+    else {
+        $errorMessage = "Please enter a time and bookmark text";
+        echo $errorMessage;
+    }
+    ?>
+
+    <!-- Worry about this later...
+                               Search:
+    <input id="inputSearchBookmark" type="text" />
+    <hr /> -->
+
+    <!-- bookmarks table -->
+
+         <table id="tableBookmark" class="table table-striped">
+    <thead>
+    <tr>
+    <th>Time</th>
+    <th>Bookmark Text</th>
+    <td>id</td>
+    </tr>
+    </thead>
+<?php
+    global $wpdb;
+    $table_plugin = $wpdb->prefix . "pcpbplugin";
+    $table_title  = $wpdb->prefix . "pcpbptitle";
+
+    if(!isset($_COOKIE[$episodeNumber])) {
+        $episodeNumber = $_COOKIE['episodeNumber'];
+    } else {
+        $episodeNumber = "default";
+    }
+
+    $bookmarks = $wpdb->get_results("SELECT * FROM $table_plugin WHERE episodeNumber = " . 
+                                    $episodeNumber . " ORDER BY startTime DESC");
+
+    $title = $wpdb->get_row("SELECT * FROM $table_title WHERE episodeNumber = " .
+                            $episodeNumber);
+
+    echo "<br/>" . $title->episodeTitle;
+
+    foreach ( $bookmarks as $bookmark ) 
+    {
+        echo "<tr>";
+        echo "<td>" . $bookmark->startTime . "</td>";
+        echo "<td>" . $bookmark->text . "</td>";
+        echo "<td>" . $bookmark->id . "</td>";
+        echo "</tr>";
+    }
+
+    ?>
+    </table>
+
+    </div>
+
+    <script>
+    console.log("<?php global $wpdb; echo $wpdb->prefix ?>");
+    </script>
+
+    <div style="visibility:hidden;">
+    <div id="dialog-message" title="Basic dialog">
+    <p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p>
+    </div>
+    </div>
+
+    </html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    </head>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.11.3/jquery-ui.min.js"></script>
+<?php $dir = plugins_url() . "/podcastpop" . "/js.js"; ?>
+    <script src="<?php echo $dir ?>"></script>
 
 
-      <?php
-   }
+<?php
+}
 
 ?>
